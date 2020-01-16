1fc416c06162dfda4312bd29439c3ce97fb911fd
Lindsey Henager <lindseyhenager@lindseys-air.attlocal.net>
2020-01-15 16:34:34 -0800

    solved makeSmartPerson and getName

--- index.js
+++ index.js
@@ -48,6 +48,7 @@ function makePersonObject(id, name, email) {
   }
 
   /* code here */
+
 }
 makePersonObject(29, "Lindsey", "lhenager09@gmail.com");
 /**
@@ -63,10 +64,16 @@ makePersonObject(29, "Lindsey", "lhenager09@gmail.com");
  * passing { id: 1, name: 'Leia', email: 'leia@leia.com` } as the argument,
  * the returned value should look like `Hello, my name is Leia`.
 */
-function getName(/* code here */) {
-  /* code here */
-}
+const user = {id: 1, name: "Leia", email: "leia@leia.com"};
 
+function getName(userObject) {
+  /* code here */
+
+  return "Hello my name is" + " " + userObject.name;
+  
+
+}
+getName(user.name);
 /**
  * ### Challenge `makeSmartPerson`
  * 
@@ -80,11 +87,21 @@ function getName(/* code here */) {
  *         and returns a string like `Hello, my name is {name}`.
  *         where `{name}` is the name passed into `makeSmartPerson`.
 */
-function makeSmartPerson(/* code here */) {
+function makeSmartPerson(name) {
   /* code here */
+  return {
+    name: name,
+    speak: function speak (name) {
+      return "Hello my name is" + " " + name;
+    }
+    sum: function add (num1, num2) {
+      return num1 + num2;
+    }
+  }
+
 }
 
-
+makeSmartPerson("Lindsey");
 
 
 
@@ -142,10 +159,32 @@ function get3rdCar(inventory) {
  * For example, if getCarInfoByIndex is invoked with the inventory and the number 0,
  * it will return `This is a Lincoln Navigator`.
 */
-function getCarInfoByIndex(inventory, index) {
-  /* code here */
-}
+var inventory = [
+  { id: 1, car_make: "Lincoln", car_model: "Navigator", car_year: 2009 },
+  { id: 2, car_make: "Mazda", car_model: "Miata MX-5", car_year: 2001 },
+  { id: 3, car_make: "Land Rover", car_model: "Defender Ice Edition", car_year: 2010 },
+  { id: 4, car_make: "Honda", car_model: "Accord", car_year: 1983 },
+  { id: 5, car_make: "Mitsubishi", car_model: "Galant", car_year: 1990 },
+  { id: 6, car_make: "Honda", car_model: "Accord", car_year: 1995 },
+  { id: 7, car_make: "Smart", car_model: "Fortwo", car_year: 2009 },
+  { id: 8, car_make: "Audi", car_model: "4000CS Quattro", car_year: 1987 },
+  { id: 9, car_make: "Ford", car_model: "Windstar", car_year: 1996 },
+  { id: 10, car_make: "Mercedes-Benz", car_model: "E-Class", car_year: 2000 },
+  { id: 11, car_make: "Infiniti", car_model: "G35", car_year: 2004 },
+  { id: 12, car_make: "Lotus", car_model: "Esprit", car_year: 2004 },
+  { id: 13, car_make: "Chevrolet", car_model: "Cavalier", car_year: 1997 },
+  { id: 14, car_make: "Dodge", car_model: "Ram Van 1500", car_year: 1999 }
+  /// ... Truncated
+]
 
+function getCarInfoByIndex(inventoryArr, index) {
+  /* code here */
+  const carInfo = inventoryArr.find((item, index) => {
+    return index === 8 // we use 2 because index is zero-based.
+  })
+  return `The car is a ${carInfo.car_make} ${carInfo.car_model}`
+}
+getCarInfoByIndex(inventory[8]);
 /**
  * ### Challenge `getLastCarInfo`
  * 
